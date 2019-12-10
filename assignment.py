43c26345ddc4f99ae42324c8c71f829ca2fabbda
cheungs01 <44142677+cheungs01@users.noreply.github.com>
2019-12-10 15:43:38 -0500

    Add files via upload

--- assignment.py
+++ assignment.py
@@ -2,7 +2,7 @@ import os
 import sys
 import gym
 import gym_anytrading
-from pylab import *
+import pylab
 import numpy as np
 import tensorflow as tf
 from reinforce import Reinforce
@@ -19,21 +19,35 @@ parser.add_argument('--mode', type=str, default='REINFORCE',
 					help='Can be "REINFORCE" or "REINFORCE_BASELINE" or "DQN"')
 args = parser.parse_args()
 
-def visualize_data(total_rewards):
+def visualize_data_rewards(total_rewards, runtype):
 	"""
 	Takes in array of rewards from each episode, visualizes reward over episodes.
 
 	:param rewards: List of rewards from all episodes
 	"""
-
-	x_values = arange(0, len(total_rewards), 1)
+	x_values = pylab.arange(0, len(total_rewards), 1)
 	y_values = total_rewards
-	plot(x_values, y_values)
-	xlabel('episodes')
-	ylabel('cumulative rewards')
-	title('Reward by Episode')
-	grid(True)
-	show()
+	pylab.plot(x_values, y_values)
+	pylab.xlabel('episodes')
+	pylab.ylabel('cumulative rewards')
+	pylab.title(runtype + ' Reward by Episode')
+	pylab.grid(True)
+	pylab.savefig(runtype + "_rewards.png")
+	
+def visualize_data_profits(total_profits, runtype):
+	"""
+	Takes in array of rewards from each episode, visualizes reward over episodes.
+
+	:param rewards: List of rewards from all episodes
+	"""
+	x_values = pylab.arange(0, len(total_rewards), 1)
+	y_values = total_rewards
+	pylab.plot(x_values, y_values)
+	pylab.xlabel('episodes')
+	pylab.ylabel('cumulative profits')
+	pylab.title(runtype + ' Profit by Episode')
+	pylab.grid(True)
+	pylab.savefig(runtype + "_profits.png")
 
 
 def discount(rewards, discount_factor=.99):
@@ -80,7 +94,7 @@ def generate_trajectory(env, model):
 		rewards.append(rwd)
 	print("Information:",info)
 	print("Max profit:",env.max_possible_profit())
-	return states, actions, rewards
+	return states, actions, rewards, info
 
 def train_dqn(env, model, iteration): 
 	epsilon = model.E / (iteration + model.E)
@@ -107,7 +121,25 @@ def train_dqn(env, model, iteration):
 		reward_sum += rwd
 	print("Information:",info)
 	print("Max profit:",env.max_possible_profit())
-	return reward_sum
+	return reward_sum, info.total_profit
+
+def random_call(env): 
+	state = env.reset()
+	rewards = []
+	done = False
+	info = 1
+	while not done: 
+		next_action = env.action_space.sample()
+		next_state, reward, done, info = env.step(next_action)
+		rewards.append(reward)
+		state = next_state
+	reward_sum = 0
+	for rwd in rewards: 
+		reward_sum += rwd
+	print("Information:",info)
+	print("Max profit:",env.max_possible_profit())
+	return reward_sum, info.total_profit
+	
 
 def train(env, model):
 	"""
@@ -125,7 +157,7 @@ def train(env, model):
 	# 1) Use generate trajectory to run an episode and get states, actions, and rewards.
 	# 2) Compute discounted rewards.
 	# 3) Compute the loss from the model and run backpropagation on the model.
-	states, actions, rewards = generate_trajectory(env, model) # Get states, actions, and rewards from one iteration
+	states, actions, rewards, info = generate_trajectory(env, model) # Get states, actions, and rewards from one iteration
 	with tf.GradientTape() as tape:
 		loss = model.loss(tf.cast(tf.convert_to_tensor(states),dtype=tf.float32), tf.convert_to_tensor(actions), tf.cast(tf.convert_to_tensor(discount(rewards)),dtype=tf.float32))
 	# Gets the gradients for this batch
@@ -135,7 +167,7 @@ def train(env, model):
 	reward_sum = 0
 	for i in rewards:
 		reward_sum+=i # Adds rewards
-	return reward_sum
+	return reward_sum, info.total_profit
 
 def main():
 
@@ -156,28 +188,38 @@ def main():
 	# 2) Append the total reward of the episode into a list keeping track of all of the rewards. 
 	# 3) After training, print the average of the last 50 rewards you've collected.
 	rewards = []
+	profits = []
 	if args.mode == "REINFORCE" or args.mode == "REINFORCE_BASELINE": 
 		try:
 			with tf.device('/device:' + args.device):
 				for i in range(650):
 					print(i)
-					reward = train(env, model)
+					reward, profit = train(env, model)
 					rewards.append(reward)
+					profits.append(profit)
 		except RuntimeError as e:
 			print(e)
-	else: 
+	else if args.mode == "DQN":
 		try:
 			with tf.device('/device:' + args.device):
 				print(args.device)
 				for i in range(1000): 
 					print(i)
-					reward = train_dqn(env, model, i)
+					reward, profit = train_dqn(env, model, i)
 					rewards.append(reward)
+					profits.append(profit)
 		except RuntimeError as e:
 			print(e)
+	else: 
+		for i in range(1000): 
+			print(i)
+			reward, profit = random_call(env)
+			rewards.append(reward)
+			profits.append(profit)
 	print("Average of last 50 rewards:",tf.reduce_mean(rewards[-50:])) # Prints average of final 50 rewards
 	# TODO: Visualize your rewards.
-	visualize_data(rewards)
+	visualize_data_rewards(rewards, args.mode)
+	visualize_data_profits(profits, args.mode)
 
 if __name__ == '__main__':
 	main()
